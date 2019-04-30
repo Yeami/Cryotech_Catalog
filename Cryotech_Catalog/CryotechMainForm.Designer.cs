2c1664eab7d4b78dae2471b4322722c84eb3b05f
Yeami <nik2x0@gmail.com>
2019-04-30 04:02:27 +0300

    Implemented Main Form Design

--- Cryotech_Catalog/CryotechMainForm.Designer.cs
+++ Cryotech_Catalog/CryotechMainForm.Designer.cs
@@ -28,20 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.DeviceTabControl = new MetroFramework.Controls.MetroTabControl();
+            this.FridgeTabPage = new System.Windows.Forms.TabPage();
+            this.FreezerTabPage = new System.Windows.Forms.TabPage();
+            this.AddFreezerTile = new MetroFramework.Controls.MetroTile();
+            this.AddFridgeTile = new MetroFramework.Controls.MetroTile();
+            this.SaveDataTile = new MetroFramework.Controls.MetroTile();
+            this.DeviceViewPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.DeviceTabControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // DeviceTabControl
+            // 
+            this.DeviceTabControl.Controls.Add(this.FridgeTabPage);
+            this.DeviceTabControl.Controls.Add(this.FreezerTabPage);
+            this.DeviceTabControl.Location = new System.Drawing.Point(23, 63);
+            this.DeviceTabControl.Name = "DeviceTabControl";
+            this.DeviceTabControl.SelectedIndex = 1;
+            this.DeviceTabControl.Size = new System.Drawing.Size(272, 564);
+            this.DeviceTabControl.TabIndex = 0;
+            this.DeviceTabControl.UseSelectable = true;
+            // 
+            // FridgeTabPage
+            // 
+            this.FridgeTabPage.Location = new System.Drawing.Point(4, 38);
+            this.FridgeTabPage.Name = "FridgeTabPage";
+            this.FridgeTabPage.Size = new System.Drawing.Size(264, 522);
+            this.FridgeTabPage.TabIndex = 0;
+            this.FridgeTabPage.Text = "Refrigerators";
+            // 
+            // FreezerTabPage
+            // 
+            this.FreezerTabPage.Location = new System.Drawing.Point(4, 38);
+            this.FreezerTabPage.Name = "FreezerTabPage";
+            this.FreezerTabPage.Size = new System.Drawing.Size(264, 522);
+            this.FreezerTabPage.TabIndex = 1;
+            this.FreezerTabPage.Text = "Freezers";
+            // 
+            // AddFreezerTile
+            // 
+            this.AddFreezerTile.ActiveControl = null;
+            this.AddFreezerTile.Location = new System.Drawing.Point(553, 52);
+            this.AddFreezerTile.Name = "AddFreezerTile";
+            this.AddFreezerTile.Size = new System.Drawing.Size(205, 43);
+            this.AddFreezerTile.TabIndex = 3;
+            this.AddFreezerTile.Text = "Add Freezer";
+            this.AddFreezerTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AddFreezerTile.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Regular;
+            this.AddFreezerTile.UseSelectable = true;
+            this.AddFreezerTile.Click += new System.EventHandler(this.AddFreezerTile_Click);
+            // 
+            // AddFridgeTile
+            // 
+            this.AddFridgeTile.ActiveControl = null;
+            this.AddFridgeTile.Location = new System.Drawing.Point(342, 52);
+            this.AddFridgeTile.Name = "AddFridgeTile";
+            this.AddFridgeTile.Size = new System.Drawing.Size(205, 43);
+            this.AddFridgeTile.TabIndex = 4;
+            this.AddFridgeTile.Text = "Add Fridge";
+            this.AddFridgeTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AddFridgeTile.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Regular;
+            this.AddFridgeTile.UseSelectable = true;
+            this.AddFridgeTile.Click += new System.EventHandler(this.AddFridgeTile_Click);
+            // 
+            // SaveDataTile
+            // 
+            this.SaveDataTile.ActiveControl = null;
+            this.SaveDataTile.Location = new System.Drawing.Point(764, 52);
+            this.SaveDataTile.Name = "SaveDataTile";
+            this.SaveDataTile.Size = new System.Drawing.Size(205, 43);
+            this.SaveDataTile.Style = MetroFramework.MetroColorStyle.Orange;
+            this.SaveDataTile.TabIndex = 5;
+            this.SaveDataTile.Text = "Save Data";
+            this.SaveDataTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SaveDataTile.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Regular;
+            this.SaveDataTile.UseSelectable = true;
+            this.SaveDataTile.Click += new System.EventHandler(this.SaveDataTile_Click);
+            // 
+            // DeviceViewPanel
+            // 
+            this.DeviceViewPanel.AutoScroll = true;
+            this.DeviceViewPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.DeviceViewPanel.Location = new System.Drawing.Point(342, 102);
+            this.DeviceViewPanel.Name = "DeviceViewPanel";
+            this.DeviceViewPanel.Size = new System.Drawing.Size(835, 521);
+            this.DeviceViewPanel.TabIndex = 8;
+            this.DeviceViewPanel.WrapContents = false;
             // 
             // CryotechMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 650);
+            this.Controls.Add(this.DeviceViewPanel);
+            this.Controls.Add(this.SaveDataTile);
+            this.Controls.Add(this.AddFridgeTile);
+            this.Controls.Add(this.AddFreezerTile);
+            this.Controls.Add(this.DeviceTabControl);
+            this.MaximizeBox = false;
             this.Name = "CryotechMainForm";
             this.Text = "Cryotech Catalog";
+            this.DeviceTabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTabControl DeviceTabControl;
+        private System.Windows.Forms.TabPage FridgeTabPage;
+        private System.Windows.Forms.TabPage FreezerTabPage;
+        private MetroFramework.Controls.MetroTile AddFreezerTile;
+        private MetroFramework.Controls.MetroTile AddFridgeTile;
+        private MetroFramework.Controls.MetroTile SaveDataTile;
+        private System.Windows.Forms.FlowLayoutPanel DeviceViewPanel;
     }
 }
 
--- Cryotech_Catalog/CryotechMainForm.cs
+++ Cryotech_Catalog/CryotechMainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cryotech_Catalog.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,47 @@ namespace Cryotech_Catalog
 {
     public partial class CryotechMainForm : MetroFramework.Forms.MetroForm
     {
+        List<Device> Devices = new List<Device>();
+
         public CryotechMainForm()
         {
             InitializeComponent();
+        }
+
+        private void AddFridgeTile_Click(object sender, EventArgs e)
+        {
+            Fridge NewFridge = new Fridge();
+
+            using (AddNewFridge AddFridgeForm = new AddNewFridge(NewFridge))
+            {
+                if (AddFridgeForm.ShowDialog() == DialogResult.OK)
+                {
+                    Devices.Add(NewFridge);
+                    MessageBox.Show("Fridge Added Successfully");
+
+                    FridgeTemplate FridgeUserControl = new FridgeTemplate(NewFridge);
+                    DeviceViewPanel.Controls.Add(FridgeUserControl);
+                }
+            }
+        }
+
+        private void AddFreezerTile_Click(object sender, EventArgs e)
+        {
+            Freezer NewFreezer = new Freezer();
+
+            using (AddNewFreezer AddFreezerForm = new AddNewFreezer(NewFreezer))
+            {
+                if (AddFreezerForm.ShowDialog() == DialogResult.OK)
+                {
+                    Devices.Add(NewFreezer);
+                    MessageBox.Show("Freezer Added Successfully");
+                }
+            }
+        }
+
+        private void SaveDataTile_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
