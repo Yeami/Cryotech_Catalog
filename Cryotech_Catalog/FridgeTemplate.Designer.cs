c0a69486736e8a98b3b1560601356363420eefa6
Yeami <nik2x0@gmail.com>
2019-04-30 04:14:37 +0300

    Implemented Fridge User Control for displaying Fridge Object data in FlowLayoutPanel

---
+++ Cryotech_Catalog/FridgeTemplate.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Cryotech_Catalog
+{
+    partial class FridgeTemplate
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FridgeBackgroundPanel = new MetroFramework.Controls.MetroPanel();
+            this.WeightInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.DimensionsInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.ControlTypeInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.CompressorsAmountInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.FridgeTypeInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.FreezerUsefulVolumeInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.FridgeUsefulVolumeInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.ColorInfoLabel = new MetroFramework.Controls.MetroLabel();
+            this.WeightLabel = new MetroFramework.Controls.MetroLabel();
+            this.DimensionsLabel = new MetroFramework.Controls.MetroLabel();
+            this.ControlTypeLabel = new MetroFramework.Controls.MetroLabel();
+            this.CompressorsAmountLabel = new MetroFramework.Controls.MetroLabel();
+            this.FridgeTypeLabel = new MetroFramework.Controls.MetroLabel();
+            this.FreezerUsefulVolumeLabel = new MetroFramework.Controls.MetroLabel();
+            this.FridgeUsefulVolumeLabel = new MetroFramework.Controls.MetroLabel();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.FridgeLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ColorLabel = new MetroFramework.Controls.MetroLabel();
+            this.PriceLabel = new System.Windows.Forms.Label();
+            this.FridgeBackgroundPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FridgeBackgroundPanel
+            // 
+            this.FridgeBackgroundPanel.Controls.Add(this.PriceLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.WeightInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.DimensionsInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.ControlTypeInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.CompressorsAmountInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.FridgeTypeInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.FreezerUsefulVolumeInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.FridgeUsefulVolumeInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.ColorInfoLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.WeightLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.DimensionsLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.ControlTypeLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.CompressorsAmountLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.FridgeTypeLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.FreezerUsefulVolumeLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.FridgeUsefulVolumeLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.TitleLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.FridgeLabel);
+            this.FridgeBackgroundPanel.Controls.Add(this.pictureBox1);
+            this.FridgeBackgroundPanel.Controls.Add(this.ColorLabel);
+            this.FridgeBackgroundPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FridgeBackgroundPanel.HorizontalScrollbarBarColor = true;
+            this.FridgeBackgroundPanel.HorizontalScrollbarHighlightOnWheel = false;
+            this.FridgeBackgroundPanel.HorizontalScrollbarSize = 10;
+            this.FridgeBackgroundPanel.Location = new System.Drawing.Point(0, 0);
+            this.FridgeBackgroundPanel.Name = "FridgeBackgroundPanel";
+            this.FridgeBackgroundPanel.Size = new System.Drawing.Size(806, 266);
+            this.FridgeBackgroundPanel.TabIndex = 0;
+            this.FridgeBackgroundPanel.VerticalScrollbarBarColor = true;
+            this.FridgeBackgroundPanel.VerticalScrollbarHighlightOnWheel = false;
+            this.FridgeBackgroundPanel.VerticalScrollbarSize = 10;
+            // 
+            // WeightInfoLabel
+            // 
+            this.WeightInfoLabel.AutoSize = true;
+            this.WeightInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.WeightInfoLabel.Location = new System.Drawing.Point(294, 225);
+            this.WeightInfoLabel.Name = "WeightInfoLabel";
+            this.WeightInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.WeightInfoLabel.TabIndex = 23;
+            this.WeightInfoLabel.Text = "Info";
+            // 
+            // DimensionsInfoLabel
+            // 
+            this.DimensionsInfoLabel.AutoSize = true;
+            this.DimensionsInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.DimensionsInfoLabel.Location = new System.Drawing.Point(400, 199);
+            this.DimensionsInfoLabel.Name = "DimensionsInfoLabel";
+            this.DimensionsInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.DimensionsInfoLabel.TabIndex = 22;
+            this.DimensionsInfoLabel.Text = "Info";
+            // 
+            // ControlTypeInfoLabel
+            // 
+            this.ControlTypeInfoLabel.AutoSize = true;
+            this.ControlTypeInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.ControlTypeInfoLabel.Location = new System.Drawing.Point(336, 174);
+            this.ControlTypeInfoLabel.Name = "ControlTypeInfoLabel";
+            this.ControlTypeInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.ControlTypeInfoLabel.TabIndex = 21;
+            this.ControlTypeInfoLabel.Text = "Info";
+            // 
+            // CompressorsAmountInfoLabel
+            // 
+            this.CompressorsAmountInfoLabel.AutoSize = true;
+            this.CompressorsAmountInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.CompressorsAmountInfoLabel.Location = new System.Drawing.Point(410, 149);
+            this.CompressorsAmountInfoLabel.Name = "CompressorsAmountInfoLabel";
+            this.CompressorsAmountInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.CompressorsAmountInfoLabel.TabIndex = 20;
+            this.CompressorsAmountInfoLabel.Text = "Info";
+            // 
+            // FridgeTypeInfoLabel
+            // 
+            this.FridgeTypeInfoLabel.AutoSize = true;
+            this.FridgeTypeInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.FridgeTypeInfoLabel.Location = new System.Drawing.Point(327, 124);
+            this.FridgeTypeInfoLabel.Name = "FridgeTypeInfoLabel";
+            this.FridgeTypeInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.FridgeTypeInfoLabel.TabIndex = 19;
+            this.FridgeTypeInfoLabel.Text = "Info";
+            // 
+            // FreezerUsefulVolumeInfoLabel
+            // 
+            this.FreezerUsefulVolumeInfoLabel.AutoSize = true;
+            this.FreezerUsefulVolumeInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.FreezerUsefulVolumeInfoLabel.Location = new System.Drawing.Point(410, 99);
+            this.FreezerUsefulVolumeInfoLabel.Name = "FreezerUsefulVolumeInfoLabel";
+            this.FreezerUsefulVolumeInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.FreezerUsefulVolumeInfoLabel.TabIndex = 18;
+            this.FreezerUsefulVolumeInfoLabel.Text = "Info";
+            // 
+            // FridgeUsefulVolumeInfoLabel
+            // 
+            this.FridgeUsefulVolumeInfoLabel.AutoSize = true;
+            this.FridgeUsefulVolumeInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.FridgeUsefulVolumeInfoLabel.Location = new System.Drawing.Point(402, 74);
+            this.FridgeUsefulVolumeInfoLabel.Name = "FridgeUsefulVolumeInfoLabel";
+            this.FridgeUsefulVolumeInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.FridgeUsefulVolumeInfoLabel.TabIndex = 17;
+            this.FridgeUsefulVolumeInfoLabel.Text = "Info";
+            // 
+            // ColorInfoLabel
+            // 
+            this.ColorInfoLabel.AutoSize = true;
+            this.ColorInfoLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.ColorInfoLabel.Location = new System.Drawing.Point(288, 50);
+            this.ColorInfoLabel.Name = "ColorInfoLabel";
+            this.ColorInfoLabel.Size = new System.Drawing.Size(41, 25);
+            this.ColorInfoLabel.TabIndex = 16;
+            this.ColorInfoLabel.Text = "Info";
+            // 
+            // WeightLabel
+            // 
+            this.WeightLabel.AutoSize = true;
+            this.WeightLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.WeightLabel.Location = new System.Drawing.Point(219, 224);
+            this.WeightLabel.Name = "WeightLabel";
+            this.WeightLabel.Size = new System.Drawing.Size(69, 25);
+            this.WeightLabel.TabIndex = 14;
+            this.WeightLabel.Text = "Weight:";
+            // 
+            // DimensionsLabel
+            // 
+            this.DimensionsLabel.AutoSize = true;
+            this.DimensionsLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.DimensionsLabel.Location = new System.Drawing.Point(219, 199);
+            this.DimensionsLabel.Name = "DimensionsLabel";
+            this.DimensionsLabel.Size = new System.Drawing.Size(175, 25);
+            this.DimensionsLabel.TabIndex = 13;
+            this.DimensionsLabel.Text = "Dimensions (HxWxD):";
+            // 
+            // ControlTypeLabel
+            // 
+            this.ControlTypeLabel.AutoSize = true;
+            this.ControlTypeLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.ControlTypeLabel.Location = new System.Drawing.Point(219, 174);
+            this.ControlTypeLabel.Name = "ControlTypeLabel";
+            this.ControlTypeLabel.Size = new System.Drawing.Size(111, 25);
+            this.ControlTypeLabel.TabIndex = 12;
+            this.ControlTypeLabel.Text = "Control Type:";
+            // 
+            // CompressorsAmountLabel
+            // 
+            this.CompressorsAmountLabel.AutoSize = true;
+            this.CompressorsAmountLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.CompressorsAmountLabel.Location = new System.Drawing.Point(219, 149);
+            this.CompressorsAmountLabel.Name = "CompressorsAmountLabel";
+            this.CompressorsAmountLabel.Size = new System.Drawing.Size(185, 25);
+            this.CompressorsAmountLabel.TabIndex = 11;
+            this.CompressorsAmountLabel.Text = "Compressors Amount: ";
+            // 
+            // FridgeTypeLabel
+            // 
+            this.FridgeTypeLabel.AutoSize = true;
+            this.FridgeTypeLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.FridgeTypeLabel.Location = new System.Drawing.Point(219, 124);
+            this.FridgeTypeLabel.Name = "FridgeTypeLabel";
+            this.FridgeTypeLabel.Size = new System.Drawing.Size(102, 25);
+            this.FridgeTypeLabel.TabIndex = 10;
+            this.FridgeTypeLabel.Text = "Fridge Type:";
+            // 
+            // FreezerUsefulVolumeLabel
+            // 
+            this.FreezerUsefulVolumeLabel.AutoSize = true;
+            this.FreezerUsefulVolumeLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.FreezerUsefulVolumeLabel.Location = new System.Drawing.Point(219, 99);
+            this.FreezerUsefulVolumeLabel.Name = "FreezerUsefulVolumeLabel";
+            this.FreezerUsefulVolumeLabel.Size = new System.Drawing.Size(185, 25);
+            this.FreezerUsefulVolumeLabel.TabIndex = 9;
+            this.FreezerUsefulVolumeLabel.Text = "Freezer Useful Volume:";
+            // 
+            // FridgeUsefulVolumeLabel
+            // 
+            this.FridgeUsefulVolumeLabel.AutoSize = true;
+            this.FridgeUsefulVolumeLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.FridgeUsefulVolumeLabel.Location = new System.Drawing.Point(219, 74);
+            this.FridgeUsefulVolumeLabel.Name = "FridgeUsefulVolumeLabel";
+            this.FridgeUsefulVolumeLabel.Size = new System.Drawing.Size(177, 25);
+            this.FridgeUsefulVolumeLabel.TabIndex = 8;
+            this.FridgeUsefulVolumeLabel.Text = "Fridge Useful Volume:";
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.BackColor = System.Drawing.Color.White;
+            this.TitleLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(93, 7);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(152, 37);
+            this.TitleLabel.TabIndex = 7;
+            this.TitleLabel.Text = "TITLELABEL";
+            // 
+            // FridgeLabel
+            // 
+            this.FridgeLabel.AutoSize = true;
+            this.FridgeLabel.BackColor = System.Drawing.Color.White;
+            this.FridgeLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FridgeLabel.Location = new System.Drawing.Point(5, 7);
+            this.FridgeLabel.Name = "FridgeLabel";
+            this.FridgeLabel.Size = new System.Drawing.Size(91, 37);
+            this.FridgeLabel.TabIndex = 6;
+            this.FridgeLabel.Text = "Fridge";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ColorLabel
+            // 
+            this.ColorLabel.AutoSize = true;
+            this.ColorLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.ColorLabel.Location = new System.Drawing.Point(219, 49);
+            this.ColorLabel.Name = "ColorLabel";
+            this.ColorLabel.Size = new System.Drawing.Size(63, 25);
+            this.ColorLabel.TabIndex = 4;
+            this.ColorLabel.Text = "Color: ";
+            // 
+            // PriceLabel
+            // 
+            this.PriceLabel.AutoSize = true;
+            this.PriceLabel.BackColor = System.Drawing.Color.White;
+            this.PriceLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PriceLabel.Location = new System.Drawing.Point(637, 7);
+            this.PriceLabel.Name = "PriceLabel";
+            this.PriceLabel.Size = new System.Drawing.Size(164, 37);
+            this.PriceLabel.TabIndex = 24;
+            this.PriceLabel.Text = "PRICELABEL";
+            // 
+            // FridgeTemplate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.FridgeBackgroundPanel);
+            this.Name = "FridgeTemplate";
+            this.Size = new System.Drawing.Size(806, 266);
+            this.FridgeBackgroundPanel.ResumeLayout(false);
+            this.FridgeBackgroundPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel FridgeBackgroundPanel;
+        private System.Windows.Forms.Label FridgeLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel ColorLabel;
+        private System.Windows.Forms.Label TitleLabel;
+        private MetroFramework.Controls.MetroLabel FridgeTypeLabel;
+        private MetroFramework.Controls.MetroLabel FreezerUsefulVolumeLabel;
+        private MetroFramework.Controls.MetroLabel FridgeUsefulVolumeLabel;
+        private MetroFramework.Controls.MetroLabel WeightLabel;
+        private MetroFramework.Controls.MetroLabel DimensionsLabel;
+        private MetroFramework.Controls.MetroLabel ControlTypeLabel;
+        private MetroFramework.Controls.MetroLabel CompressorsAmountLabel;
+        private MetroFramework.Controls.MetroLabel WeightInfoLabel;
+        private MetroFramework.Controls.MetroLabel DimensionsInfoLabel;
+        private MetroFramework.Controls.MetroLabel ControlTypeInfoLabel;
+        private MetroFramework.Controls.MetroLabel CompressorsAmountInfoLabel;
+        private MetroFramework.Controls.MetroLabel FridgeTypeInfoLabel;
+        private MetroFramework.Controls.MetroLabel FreezerUsefulVolumeInfoLabel;
+        private MetroFramework.Controls.MetroLabel FridgeUsefulVolumeInfoLabel;
+        private MetroFramework.Controls.MetroLabel ColorInfoLabel;
+        private System.Windows.Forms.Label PriceLabel;
+    }
+}
---
+++ Cryotech_Catalog/FridgeTemplate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cryotech_Catalog.Classes;
+
+namespace Cryotech_Catalog
+{
+    public partial class FridgeTemplate : UserControl
+    {
+        public FridgeTemplate(Fridge NewFridge)
+        {
+            InitializeComponent();
+
+            TitleLabel.Text = NewFridge.TitleToString();
+            ColorInfoLabel.Text = NewFridge.Color;
+            FridgeUsefulVolumeInfoLabel.Text = Convert.ToString(NewFridge.FridgeUsefulVolume) + " L";
+            FreezerUsefulVolumeInfoLabel.Text = Convert.ToString(NewFridge.FreezerUsefulVolume) + " L";
+            FridgeTypeInfoLabel.Text = Convert.ToString(NewFridge.DeviceType);
+            CompressorsAmountInfoLabel.Text = Convert.ToString(NewFridge.CompressorsAmount);
+            ControlTypeInfoLabel.Text = Convert.ToString(NewFridge.ControlType);
+            DimensionsInfoLabel.Text = NewFridge.DimensionsToString() + " sm";
+            WeightInfoLabel.Text = Convert.ToString(NewFridge.Weight) + " kg";
+            PriceLabel.Text = Convert.ToString(NewFridge.Price) + " uah";
+            PriceLabel.ForeColor = System.Drawing.Color.Red;
+        }
+    }
+}
